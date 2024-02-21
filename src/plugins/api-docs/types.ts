1e09945904fd6b6b676ad02dd564c57525c50fe4
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-02-21 10:29:43 -0500

    Refactor so that the typedoc plugin can be used outside of unplugin

---
+++ src/plugins/api-docs/types.ts
@@ -0,0 +1,12 @@
+export interface APIDocsOptions {
+  /**
+* List of packages to generate api docs for
+*/
+  packages: string[];
+
+  /**
+* Destination folder to place the api docs json files in.
+  * Defaults to "docs"
+*/
+  dest?: string;
+}
