b2f9cf9ec2ce79e66f9e1f7671ca2c8de55517c4
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-03-04 22:41:10 -0500

    Try out vite -- it behaves worse 😅

--- docs-app/ember-cli-build.js
+++ docs-app/ember-cli-build.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const { prebuild } = require('@embroider/compat');
 
 module.exports = async function (defaults) {
   const app = new EmberApp(defaults, {
@@ -10,39 +11,46 @@ module.exports = async function (defaults) {
     },
   });
 
-  const { Webpack } = require('@embroider/webpack');
+  if (!process.env.WEBPACK) {
+    return prebuild(app);
+  }
 
-  const { markdownPages, apiDocs, setup } = await import('kolay/webpack');
+  if (process.env.WEBPACK) {
+    const { Webpack } = require('@embroider/webpack');
+    const { markdownPages, apiDocs, setup } = await import('kolay/webpack');
 
-  return require('@embroider/compat').compatBuild(app, Webpack, {
-    staticAddonTestSupportTrees: true,
-    staticAddonTrees: true,
-    staticHelpers: true,
-    staticModifiers: true,
-    staticComponents: true,
-    staticEmberSource: true,
-    skipBabel: [
-      {
-        package: 'qunit',
+    return require('@embroider/compat').compatBuild(app, Webpack, {
+      staticAddonTestSupportTrees: true,
+      staticAddonTrees: true,
+      staticHelpers: true,
+      staticModifiers: true,
+      staticComponents: true,
+      staticEmberSource: true,
+      skipBabel: [
+        {
+          package: 'qunit',
+        },
+      ],
+      packagerOptions: {
+        webpackConfig: {
+          devtool: 'source-map',
+          plugins: [
+            setup(),
+            markdownPages({
+              src: 'public/docs',
+              groups: [
+                {
+                  name: 'Runtime',
+                  src: '../ui/docs',
+                },
+              ],
+            }),
+            apiDocs({ packages: ['kolay', 'ember-primitives', 'ember-resources'] }),
+          ],
+        },
       },
-    ],
-    packagerOptions: {
-      webpackConfig: {
-        devtool: 'source-map',
-        plugins: [
-          setup(),
-          markdownPages({
-            src: 'public/docs',
-            groups: [
-              {
-                name: 'Runtime',
-                src: '../ui/docs',
-              },
-            ],
-          }),
-          apiDocs({ packages: ['kolay', 'ember-primitives', 'ember-resources'] }),
-        ],
-      },
-    },
-  });
+    });
+  }
+
+  throw new Error('Good luck reproducing the situation that got you here.');
 };
