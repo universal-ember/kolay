eb497437f70ba30003db27924e2cc9dadc079c70
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-03-08 09:35:58 -0500

    Yay

--- src/plugins/vite.js
+++
@@ -1,7 +0,0 @@
-import { apiDocs as unApiDocs } from './api-docs/index.js';
-import { markdownPages as unMarkdownPages } from './markdown-pages/index.js';
-import { setup as unSetup } from './setup.js';
-
-export const apiDocs = unApiDocs.vite;
-export const markdownPages = unMarkdownPages.vite;
-export const setup = unSetup.vite;
--- src/plugins/webpack.js
+++
@@ -1,7 +0,0 @@
-import { apiDocs as unApiDocs } from './api-docs/index.js';
-import { markdownPages as unMarkdownPages } from './markdown-pages/index.js';
-import { setup as unSetup } from './setup.js';
-
-export const apiDocs = unApiDocs.webpack;
-export const markdownPages = unMarkdownPages.webpack;
-export const setup = unSetup.webpack;
