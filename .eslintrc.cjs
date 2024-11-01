5623165d85672bdde440c838718ab60fb8144689
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-11-01 00:00:24 -0400

    Lints are broken

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -34,76 +34,91 @@ module.exports = {
      * Node-land
      ********************************
      */
-    ...node.overrides,
-    {
-      files: ['**/*.{ts,js}'],
-      rules: {
-        '@typescript-eslint/no-explicit-any': 'off',
-      },
-    },
-    {
-      files: ['**/*.test.{ts,js}'],
-      rules: {
-        'n/no-unpublished-import': 'off',
-      },
-    },
-    {
-      files: ['**/*.d.ts'],
-      rules: {
-        'n/no-unsupported-features/node-builtins': 'off',
-        'n/no-missing-import': 'off',
-      },
-    },
-    {
-      files: ['src/browser/**/*', '**/*.test.ts', 'src/plugins/markdown-pages/types.ts'],
-      rules: {
-        'n/no-unpublished-import': 'off',
-        'n/no-missing-import': 'off',
-        'n/no-unsupported-features': 'off',
-        'n/no-unsupported-features/node-builtins': 'off',
-      },
-    },
-    /**
-     ********************************
-     * Browser-land
-     ********************************
-     */
-    within('browser', [
-      ...ember.overrides,
+    within('src/build', [
+      ...node.overrides,
       {
-        files: ['rollup.config.mjs'],
+        files: ['**/*.{ts,js}'],
         rules: {
-          'no-console': 'off',
+          '@typescript-eslint/no-explicit-any': 'off',
         },
       },
       {
-        files: ['**/*.gts'],
-        plugins: ['ember'],
-        parser: 'ember-eslint-parser',
-      },
-      {
-        files: ['**/*.gjs'],
-        plugins: ['ember'],
-        parser: 'ember-eslint-parser',
-      },
-      {
-        files: ['**/*.ts', '**/*.gts'],
+        files: ['**/*.test.{ts,js}'],
         rules: {
-          '@typescript-eslint/no-explicit-any': 'off',
+          'n/no-unpublished-import': 'off',
         },
       },
       {
         files: ['**/*.d.ts'],
         rules: {
           'n/no-unsupported-features/node-builtins': 'off',
+          'n/no-missing-import': 'off',
         },
       },
       {
-        files: ['**/*.cjs'],
+        files: ['**/*.test.ts'],
         rules: {
+          'n/no-unpublished-import': 'off',
+          'n/no-missing-import': 'off',
           'n/no-unsupported-features': 'off',
+          'n/no-unsupported-features/node-builtins': 'off',
         },
       },
     ]),
+    /**
+     ********************************
+     * Browser-land
+     ********************************
+     */
+    within(
+      'src/browser',
+      [
+        ...ember.overrides,
+        {
+          files: ['rollup.config.mjs'],
+          rules: {
+            'no-console': 'off',
+          },
+        },
+        {
+          files: ['**/*.gts'],
+          plugins: ['ember'],
+          parser: 'ember-eslint-parser',
+        },
+        {
+          files: ['**/*.gjs'],
+          plugins: ['ember'],
+          parser: 'ember-eslint-parser',
+        },
+        {
+          files: ['**/*.ts', '**/*.gts'],
+          rules: {
+            '@typescript-eslint/no-explicit-any': 'off',
+          },
+        },
+        {
+          files: ['**/*.d.ts'],
+          rules: {
+            'n/no-unsupported-features/node-builtins': 'off',
+          },
+        },
+        {
+          files: ['**/*.cjs'],
+          rules: {
+            'n/no-unsupported-features': 'off',
+          },
+        },
+      ].filter((config) => {
+        let files = Array.isArray(config.files) ? config.files : [config.files];
+
+        let isRrelevant = files.some(
+          (f) => f.includes('src') || f.includes('gjs') || f.includes('gts')
+        );
+
+        return isRrelevant;
+      })
+    ),
   ].flat(),
 };
+
+console.log(module.exports);
--- .template-lintrc.cjs
+++ .template-lintrc.cjs
@@ -4,5 +4,6 @@ module.exports = {
   extends: 'recommended',
   rules: {
     'no-forbidden-elements': 'off',
+    'no-inline-styles': 'off',
   },
 };
