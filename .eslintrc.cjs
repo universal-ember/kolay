f9763ce1643d8cc02b39d1a4e767e99cba366ee6
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-10-31 20:24:40 -0400

    Move all the files around

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,10 +3,16 @@
 const { configs } = require('@nullvoxpopuli/eslint-configs');
 
 const node = configs.node();
+const ember = configs.ember();
 
 module.exports = {
   root: true,
   overrides: [
+    /**
+     ********************************
+     * Node-land
+     ********************************
+     */
     ...node.overrides,
     {
       files: ['**/*.{ts,js}'],
@@ -32,6 +38,46 @@ module.exports = {
       rules: {
         'n/no-unpublished-import': 'off',
         'n/no-missing-import': 'off',
+      },
+    },
+    /**
+     ********************************
+     * Browser-land
+     ********************************
+     */
+    ...ember.overrides,
+    {
+      files: ['rollup.config.mjs'],
+      rules: {
+        'no-console': 'off',
+      },
+    },
+    {
+      files: ['**/*.gts'],
+      plugins: ['ember'],
+      parser: 'ember-eslint-parser',
+    },
+    {
+      files: ['**/*.gjs'],
+      plugins: ['ember'],
+      parser: 'ember-eslint-parser',
+    },
+    {
+      files: ['**/*.ts', '**/*.gts'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+      },
+    },
+    {
+      files: ['**/*.d.ts'],
+      rules: {
+        'n/no-unsupported-features/node-builtins': 'off',
+      },
+    },
+    {
+      files: ['**/*.cjs'],
+      rules: {
+        'n/no-unsupported-features': 'off',
       },
     },
   ],
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,10 +2,11 @@
 
 module.exports = {
   printWidth: 100,
+  plugins: ['prettier-plugin-ember-template-tag'],
   overrides: [
     {
       // Lol, JavaScript
-      files: ['*.js', '*.ts', '*.cjs', '.mjs', '.cts', '.mts', '.cts'],
+      files: ['*.js', '*.ts', '*.cjs', '.mjs', '.cts', '.mts', '.cts', '.gjs', '.gts'],
       options: {
         singleQuote: true,
         trailingComma: 'es5',
