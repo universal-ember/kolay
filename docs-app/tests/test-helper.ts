fc39e41629741c7aabeab277f8f3994cf0ab4d9a
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-02-26 11:12:43 -0500

    Much better

--- docs-app/tests/test-helper.ts
+++ docs-app/tests/test-helper.ts
@@ -4,9 +4,10 @@ import { setup } from 'qunit-dom';
 import { setupEmberOnerrorValidation, start as qunitStart } from 'ember-qunit';
 
 import Application from '../src/app.ts';
-import config from '../src/config.ts';
+import config, { enterTestMode } from '../src/config.ts';
 
 export function start() {
+  enterTestMode();
   setApplication(Application.create(config.APP));
 
   setup(QUnit.assert);
